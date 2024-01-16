d8865cc675f9bed5f55b07dd918998eb13fb15fa
HARISH SEMWAL <96759566+Harishsemwal@users.noreply.github.com>
2024-01-16 21:36:08 +0530

    Time: 170 ms (69.97%), Space: 97.4 MB (73.46%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,48 @@
+class RandomizedSet {
+    vector<int> v;
+    unordered_map<int,int> mp;
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+    }
+
+    bool search(int val){
+//         is present.
+         if(mp.find(val)!=mp.end())
+            return true;
+         return false;
+
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+//         if already there , no need to add.
+        if(search(val))
+            return false;
+
+        v.push_back(val);
+        mp[val] = v.size()-1;
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+//         if not present.we can remove.
+        if(!search(val))
+            return false;
+
+        // pointer to the value.
+        auto it = mp.find(val);
+        v[it->second] = v.back();
+        v.pop_back();
+        mp[v[it->second]] = it->second;
+        mp.erase(val);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    int getRandom() {
+//         rand()%v.size() : give us index between 1 to v.size()-1
+        return v[rand()%v.size()];
+    }
+};
