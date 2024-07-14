000c9f7e05feaf42e7d63220f4bc127482de8d48
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-14 22:58:22 +0530

    Time: 0 ms (100%), Space: 10.2 MB (7.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        int i = 0;
-        for(auto x: mp){
-            int r = x.second;
-            while(r--){
-               nums[i++] = x.first; 
-            }
-        }
+        sort(nums.begin(), nums.end());
     }
 };
