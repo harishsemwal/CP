9926dbd163c9aebcf5250ae8eee7aa9f2a589437
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-14 23:04:33 +0530

    Time: 5 ms (15.91%), Space: 9.9 MB (7.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid = 0;
+        int n = nums.size();
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
