d42628b95f0a39dde8677576b2712f3c32491331
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-17 14:16:16 +0530

    Time: 59 ms (69.47%), Space: 61.7 MB (9.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int solve(int x, int y){
-        return (x * y);
+        return x*y;
     }
     int maxArea(vector<int>& height) {
         int n = height.size();
         int left = 0;
         int right = n - 1;
-
         int result = 0;
-        while(left < right){
-            result = max(result, solve(min(height[left], height[right]), (right - left)));
+        while(left <= right){
+            result = max(result, solve((min(height[left], height[right])) , (right - left)));
             if(height[left] < height[right]){
                 left++;
             }
