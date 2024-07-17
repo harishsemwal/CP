bfc0a7b08e58a97cd0ec10b077df45903f76f49a
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-17 15:49:28 +0530

    Time: 55 ms (87.1%), Space: 27.4 MB (59.87%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,31 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end()); 
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) {
-                continue;  
-            }
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]){continue;} // understand this
             int j = i + 1;
             int k = n - 1;
-            while(j < k) {
+            while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;  
-                } else if(sum > 0) {
-                    k--;  
-                } else {
-                    result.push_back({nums[i], nums[j], nums[k]});
-                    // Move the left pointer right and skip duplicates
+                if(sum < 0){
                     j++;
-                    while(j < k && nums[j] == nums[j-1]) {
-                        j++;
-                    }
-                    // Move the right pointer left and skip duplicates
+                }
+                else if(sum > 0){
                     k--;
-                    while(j < k && nums[k] == nums[k+1]) {
-                        k--;
-                    }
+                }
+                else{
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    // Not include duplicate in the answer array
+                    while(j < k && nums[j] == nums[j-1]){ j++; } // understand this
+                    while(j < k && nums[k] == nums[k+1]){ k--; } // understand this
                 }
             }
         }
-
         return result;
     }
 };
