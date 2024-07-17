6101c1afe0e043530c877f7a6a6d2bfb8318362f
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-17 15:43:53 +0530

    Time: 52 ms (92.61%), Space: 27.5 MB (58.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,33 +7,26 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end());  // Sort the array to use two pointers
-
+        sort(nums.begin(), nums.end()); 
         for(int i = 0; i < n; i++) {
             if(i > 0 && nums[i] == nums[i-1]) {
-                continue;  // Skip duplicate elements for the i-th position
+                continue;  
             }
-
             int j = i + 1;
             int k = n - 1;
-
             while(j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-
                 if(sum < 0) {
-                    j++;  // Move the left pointer right to increase the sum
+                    j++;  
                 } else if(sum > 0) {
-                    k--;  // Move the right pointer left to decrease the sum
+                    k--;  
                 } else {
-                    // Found a triplet
                     result.push_back({nums[i], nums[j], nums[k]});
-
                     // Move the left pointer right and skip duplicates
                     j++;
                     while(j < k && nums[j] == nums[j-1]) {
                         j++;
                     }
-
                     // Move the right pointer left and skip duplicates
                     k--;
                     while(j < k && nums[k] == nums[k+1]) {
