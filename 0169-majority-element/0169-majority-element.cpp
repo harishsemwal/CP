655b9cefd731cb56db754233971975fd56d38956
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-22 20:30:41 +0530

    Time: 18 ms (53.83%), Space: 27 MB (43.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> m;
-        int n = nums.size();
-        
-        for(int i : nums)
-        {
-            m[i]++;
+        int count = 0;
+        int candidate = 0;
+        for(auto x: nums){
+            if(count == 0){
+                candidate = x;
+            }
+
+            if(candidate == x){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        for(auto it : m)
-        {
-            if(it.second>n/2)
-                return it.first;
-        }
-        
-        return -1;
+        return candidate;
     }
 };
