06ae7d804cfe351cc64cf27eb02cd2b6dfec16f7
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-24 18:05:46 +0530

    Time: 166 ms (37.31%), Space: 102.1 MB (9.71%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,48 +1,53 @@
 class RandomizedSet {
-    vector<int> v;
-    unordered_map<int,int> mp;
 public:
-    /** Initialize your data structure here. */
+    vector<int> v;
+    int ind = 0;
+    unordered_map<int, int> mp;
     RandomizedSet() {
+        ind = 0;
+        v.clear();
+        mp.clear();
     }
-
-    bool search(int val){
-//         is present.
-         if(mp.find(val)!=mp.end())
-            return true;
-         return false;
-
-    }
-
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    
     bool insert(int val) {
-//         if already there , no need to add.
-        if(search(val))
+        if(mp.find(val) != mp.end()){
             return false;
-
+        }
+        mp[val] = ind++;
         v.push_back(val);
-        mp[val] = v.size()-1;
         return true;
     }
-
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    
     bool remove(int val) {
-//         if not present.we can remove.
-        if(!search(val))
+        if(mp.find(val) == mp.end()){
             return false;
+        }
 
-        // pointer to the value.
-        auto it = mp.find(val);
-        v[it->second] = v.back();
+        // Get Index
+        int i = mp[val];
+
+        //swap
+        swap(v[i], v[ind-1]);
+        mp[v[i]] = i;
+
+        //remove
         v.pop_back();
-        mp[v[it->second]] = it->second;
+        ind--;
+
         mp.erase(val);
         return true;
     }
-
-    /** Get a random element from the set. */
+    
     int getRandom() {
-//         rand()%v.size() : give us index between 1 to v.size()-1
-        return v[rand()%v.size()];
+        int i = rand()%ind;
+        return v[i];
     }
 };
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
