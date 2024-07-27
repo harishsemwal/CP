2341c3cb82524211026cbf3bb44a201b25203dfa
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-07-27 10:56:08 +0530

    Time: 19 ms (58.34%), Space: 14 MB (26.41%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -7,13 +7,9 @@ public:
         int num = 0; 
         
         for(int i = 0; i < n; i++) {
-            // If the current character is a digit, construct the number
             if(isdigit(s[i])) {
                 num = num * 10 + (s[i] - '0');
             }
-            
-            // If the current character is an operator or the end of the string,
-            // process the previous number
             if(!isdigit(s[i]) && !isspace(s[i]) || i == n - 1) {
                 if(sign == '+') {
                     st.push(num);
@@ -28,13 +24,10 @@ public:
                     st.pop();
                     st.push(top / num);
                 }
-                // Update the sign and reset the number
                 sign = s[i];
                 num = 0;
             }
         }
-
-        // Sum up all the values in the stack
         int result = 0;
         while(!st.empty()) {
             result += st.top();
