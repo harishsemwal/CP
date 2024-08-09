4dcb1111146b0b25389eb91fa94572f754a5d099
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-08-09 22:15:39 +0530

    Time: 26 ms (56.9%), Space: 22.8 MB (29.21%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,40 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     void reorderList(ListNode* head) {
         if (!head || !head->next)
             return;
-        
-        vector<int> vt;
-        ListNode* temp = head;
-        while(temp != NULL){
-            vt.push_back(temp->val);
-            temp = temp->next;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        
-        vector<int> store;
-        int n = vt.size();
-        
-        for(int i = 0;i < n/2; i++){
-            store.push_back(vt[i]);
-            store.push_back(vt[n-i-1]);
+
+       
+        ListNode* prev = nullptr;
+        ListNode* curr = slow->next;
+        slow->next = nullptr; 
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
         }
-        if (n % 2 == 1)
-            store.push_back(vt[n / 2]);
-        
-        temp = head;
-        for(auto x: store){
-            temp->val = x;
-            temp = temp->next;
+
+       
+        ListNode* first = head;
+        ListNode* second = prev;
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
         }
     }
 };
