8abd6f36e2f937b03537b0dc7a3c66015d19d961
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-08-30 22:37:36 +0530

    Time: 16 ms (46.06%), Space: 35.1 MB (44.84%) - LeetHub

--- 3533-snake-in-matrix/3533-snake-in-matrix.cpp
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int finalPositionOfSnake(int n, vector<string>& commands) {
-        int x = 0;
-        vector<vector<int>>dir(110, vector<int>(110));
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                dir[i][j] = x++;
-
-        int i = 0, j = 0;
-        for(auto com:commands)
-        {
-            if(com == "RIGHT") j++;
-            else if(com == "DOWN") i++;
-            else if(com == "LEFT") j--;
-            else if(com == "UP") i--;
+    int finalPositionOfSnake(int girdSize, vector<string>& commands) {
+        int x = 0, y = 0, n = commands.size();
+        for(int i = 0; i < n; i++){
+            string s = commands[i];
+            if(s == "UP"){
+                x--;
+            }
+            else if(s == "DOWN"){
+                x++;
+            }
+            else if(s == "RIGHT"){
+                y++;
+            }
+            else{
+                y--;
+            }
         }
-        return dir[i][j];
+        return (x * girdSize) + y;
     }
 };
