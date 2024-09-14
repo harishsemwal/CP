2535300d86983ce2b55daea83bd7f0c9abc8d470
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-09-14 13:39:48 +0530

    Time: 2 ms (47.9%), Space: 9 MB (11.11%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // using Inplace
-        int n = matrix.size();
-        for(int i = 0; i < n - 1; i++){
-            for(int j = i + 1; j < n ; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        
-        for(int i = 0; i < n; i++){
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
+    // 2nd approach -: 
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
     }
+    }
+
+    for(int i = 0; i < n; i++){
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+    }
+    
+    // 1-st approch -: 
+    // int n = matrix.size();
+    // vector < vector < int >> rotated(n, vector < int > (n, 0));
+    // for (int i = 0; i < n; i++) {
+    // for (int j = 0; j < n; j++) {
+    //     rotated[j][n - i - 1] = matrix[i][j];
+    // }
+    // }
+    // matrix = rotated;
+    // }
 };
