eb4210560841e2497b523e071afc02bfb4e07f20
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2024-12-23 16:32:07 +0530

    Time: 0 ms (100%), Space: 14.8 MB (20.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        /*
+        Time - O(N^2)
+        Space - O(1)
+
         int n = nums.size();
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
@@ -10,5 +14,28 @@ public:
             }
         }
         return {-1};
+        */
+
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        // find complement
+        for(int i = 0; i < n; i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+
+
+        /*
+        Map-:
+        complement = 2
+        key  value target = 9
+        2       1  = > return {0, 1}
+        7       1
+        
+        */
     }
 };
