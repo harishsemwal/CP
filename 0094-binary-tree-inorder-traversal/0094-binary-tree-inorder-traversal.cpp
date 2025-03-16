a024cc2bde64ce14220888b395bb33f7b2e301ac
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2025-03-16 13:32:38 +0530

    Time: 0 ms (100%), Space: 10.8 MB (88.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -6,24 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector <int> vt;
-    void inorder(TreeNode *root)
-    {
-        if(root==NULL)
-        {
-            return;
-        }
-        inorder(root->left);
-        vt.push_back(root->val);
-        inorder(root->right);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        inorder(root);
-        return vt;
+        // Morris Traversal 
+        vector<int> ans;
+        while (root) {
+            if (root->left == nullptr) {
+                ans.push_back(root->val);
+                root = root->right;
+            } else {
+                TreeNode* curr = root->left;
+                while (curr->right && curr->right != root) {
+                    curr = curr->right;
+                }
+
+                if (curr->right == nullptr) {
+                    curr->right = root;
+                    root = root->left;
+                } else {
+                    curr->right = nullptr;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
     }
 };
