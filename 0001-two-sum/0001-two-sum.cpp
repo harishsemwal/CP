578d34a00ff5e492212268a78e12ce9517e1bb04
Harish P. Semwal <96759566+harishsemwal@users.noreply.github.com>
2026-04-14 14:33:37 +0530

    Time: 3 ms (66.71%), Space: 15 MB (18.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // 1-st approach
         /*
-        Time - O(N^2)
-        Space - O(1)
-
+        vector<int> result;
         int n = nums.size();
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
-                if(nums[i] + nums[j] == target){
+                if(nums[i]+nums[j] == target){
                     return {i, j};
                 }
             }
         }
-        return {-1};
+        return {0, 0};
+
+        // 2nd approach
+        sort(nums.begin(), nums.end());
+        int i = 0; 
+        int n = nums.size();
+        ---- :- Thinking -: ----
+
         */
 
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        // find complement
-        for(int i = 0; i < n; i++){
-            int complement = target - nums[i];
-            if(mp.count(complement)){
-                return {mp[complement], i};
+        // 3rd approach
+        map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            int comp = target - nums[i];
+            if(mp.find(comp) != mp.end()){
+                return {mp[comp], i};
             }
             mp[nums[i]] = i;
         }
-        return {};
-
-
-        /*
-        Map-:
-        complement = 2
-        key  value target = 9
-        2       1  = > return {0, 1}
-        7       1
-        
-        */
+        return {0, 0};
     }
 };
